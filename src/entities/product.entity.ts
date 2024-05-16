8df8c1728a7ca8708636133c2e772a27a5e2dc0b
oier <oieruni22@gmail.com>
2024-05-16 11:53:27 +0200

    Add entity
    Adding Product Entity to use Product types in controllers and services.

--- src/entities/product.entity.ts
+++ src/entities/product.entity.ts
@@ -1,8 +1,15 @@
-export interface Product {
+import { IsNumber, IsString, IsUrl } from 'class-validator';
+
+export class Product {
   id: number;
+  @IsString()
   name: string;
+  @IsString()
   description: string;
+  @IsNumber()
   price: number;
+  @IsNumber()
   stock: number;
+  @IsUrl()
   image: string;
 }
