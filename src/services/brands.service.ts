a1ebcab095e548f1fdd9ec806ffaa928922d1a17
oier <oieruni22@gmail.com>
2024-05-16 12:34:43 +0200

    Add methods to service
    Adding CRUD methods to brands.service file.

--- src/services/brands.service.ts
+++ src/services/brands.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Brand } from 'src/entities/brand.entity';
 
 @Injectable()
-export class BrandsService {}
+export class BrandsService {
+  find() {}
+
+  findOne(id: Brand['id']) {
+    return id;
+  }
+
+  create(payload: any) {
+    return payload;
+  }
+
+  update(id: Brand['id']) {
+    return id;
+  }
+
+  delete(id: Brand['id']) {
+    return id;
+  }
+}
