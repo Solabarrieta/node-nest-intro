554375ad3e188f918bc98392ecca72107c7578c9
oier <oieruni22@gmail.com>
2024-05-16 11:57:14 +0200

    Create Pipes
    Creating pipes to parse url parameters from string to number.

---
+++ src/common/parse-int/parse-int.pipe.ts
@@ -0,0 +1,10 @@
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+
+@Injectable()
+export class ParseIntPipe implements PipeTransform {
+  transform(value: any) {
+    const val = parseInt(value, 10);
+    if (isNaN(val)) throw new BadRequestException(`${value} is not a number`);
+    return val;
+  }
+}
